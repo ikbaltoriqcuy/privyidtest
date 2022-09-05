8f3c7d4ed10a9337d7c8af3461eaafecaaea5a30
ikbaltoriqcuy <ikbalok01@gmail.com>
2022-09-05 18:14:23 +0700

    - feature :    handle deprecated chuck

--- api_helper/src/main/java/com/example/api_helper/apiservice/RetrofitAPI.kt
+++ api_helper/src/main/java/com/example/api_helper/apiservice/RetrofitAPI.kt
@@ -1,6 +1,7 @@
 package com.example.api_helper.apiservice
 
 import android.content.Context
+import android.os.Build
 import com.readystatesoftware.chuck.ChuckInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -15,8 +16,10 @@ object RetrofitAPI {
             .connectTimeout(20, TimeUnit.SECONDS)
             .readTimeout(20, TimeUnit.SECONDS)
             .writeTimeout(20, TimeUnit.SECONDS)
-            .cache(null)
-            .addInterceptor(ChuckInterceptor(context))
+            .cache(null).apply {
+                if (Build.VERSION.SDK_INT < 31)
+                    addInterceptor(ChuckInterceptor(context))
+            }
             .build()
 
         val retrofit = Retrofit.Builder()
