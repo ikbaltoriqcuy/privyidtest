f6e1470fafd74d8eb78cd9b69d7b5f934fcc9702
ikbaltoriqcuy <ikbalok01@gmail.com>
2022-09-05 08:56:36 +0700

    - feature :   add fragment home

---
+++ app/src/main/java/com/example/privytest/ui/fragment/FragmentHome.kt
@@ -0,0 +1,12 @@
+package com.example.privytest.ui.main
+
+import com.example.common_base.BaseFragment
+import com.example.privytest.R
+
+class FragmentHome : BaseFragment() {
+
+    override fun setLayoutId(): Int = R.layout.fragment_home
+
+    override fun initView() {}
+
+}
